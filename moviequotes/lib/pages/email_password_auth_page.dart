5a80e300d73e618dae2170e19e76541e93e76679
David Fisher <fisherds@rose-hulman.edu>
2024-10-17 13:34:43 -0400

    form validation

--- moviequotes/lib/pages/email_password_auth_page.dart
+++ moviequotes/lib/pages/email_password_auth_page.dart
@@ -1,5 +1,7 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:moviequotes/components/square_button.dart';
 
 class EmailPasswordAuthPage extends StatefulWidget {
   final bool isNewUser;
@@ -13,6 +15,17 @@ class EmailPasswordAuthPage extends StatefulWidget {
 }
 
 class _EmailPasswordAuthPageState extends State<EmailPasswordAuthPage> {
+  final _formKey = GlobalKey<FormState>();
+  final emailTextEditingController = TextEditingController();
+  final passwordTextEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailTextEditingController.dispose();
+    passwordTextEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +34,73 @@ class _EmailPasswordAuthPageState extends State<EmailPasswordAuthPage> {
         title: Text(
             widget.isNewUser ? "Create a New User" : "Log in an Existing User"),
       ),
-      body: Text("TODO tomorrow"),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: emailTextEditingController,
+                  validator: (value) {
+                    if (value == null || !EmailValidator.validate(value)) {
+                      return "Invalid Email Address";
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Email",
+                    hintText: "Enter a valid email address",
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  controller: passwordTextEditingController,
+                  validator: (value) {
+                    if (value == null || value.length < 6) {
+                      return "Passwords must be at least 6 characters";
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Password",
+                    hintText: "Passwords must be 6 characters or more",
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                ),
+                SquareButton(
+                  displayText:
+                      widget.isNewUser ? "Create an Account" : "Log in",
+                  onPressCallback: () {
+                    if (_formKey.currentState!.validate()) {
+                      if (widget.isNewUser) {
+                        print("Create a new account");
+                      } else {
+                        print("Log this user back in");
+                      }
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Invalid Email or Password"),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
