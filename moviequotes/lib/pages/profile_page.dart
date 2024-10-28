70ef55b450aa631e7fccbb33fb903dd4ab1a53a4
David Fisher <fisherds@rose-hulman.edu>
2024-10-28 13:50:24 -0400

    getting the User Data

--- moviequotes/lib/pages/profile_page.dart
+++ moviequotes/lib/pages/profile_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:moviequotes/managers/auth_manager.dart';
+import 'package:moviequotes/managers/user_data_document_manager%20copy.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -9,6 +13,33 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final displayNameTextEditingController = TextEditingController();
+  StreamSubscription? userDataSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    userDataSubscription = UserDataDocumentManager.instance.startListening(
+      documentId: AuthManager.instance.uid,
+      observer: () {
+        setState(() {
+          displayNameTextEditingController.text =
+              UserDataDocumentManager.instance.displayName;
+          print(
+              "TODO: Display the name: ${UserDataDocumentManager.instance.displayName}");
+          print(
+              "TODO: Display the image: ${UserDataDocumentManager.instance.imageUrl}");
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    displayNameTextEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
